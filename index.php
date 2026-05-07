b6b10c2fc67070dd30b7bef4cc23d89d9dff06d0
Klaa5 <nic.n@outlook.es>
2026-05-07 18:25:21 -0300

    Se ingresa form de login basico

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EventConnect</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+
+session_start();
+
+
+    if($_SESSION['nickName'] != null)   //Al ingresar verifico si el usuario se ha logueado.
+    {
+        header("Location: paginaPrincipal.php");
+        exit();
+    }
+    else
+    {
+        header("Location: login.php");
+        exit();
+    }
+
+
+
+?>
