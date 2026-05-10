2789f244480b76ca3be8658cf4bc6d22d3bb0e53
Klaa5 <nic.n@outlook.es>
2026-05-10 00:57:45 -0300

    Correciones y nueva funcion para registrar sala en BD. Correcciones en ingreso de datos al crear un objeto y nueva funcion con sentencia preparada

--- objetos/sala.php
+++ objetos/sala.php
@@ -2,25 +2,57 @@
 
 class Sala
 {
-    private $idSala;
+    private $idSala;            //queda en 0 si se esta almacenado en la bd, queda en N si se trae de alla
     private $titulo;
     private $descripcion;
     private $modalidad;
     private $nickNameCreador;
-    private $ubicacion = null;          //queda en null si es virtual
+    private $ubicacion;          //queda en null si es virtual
     private $fechaHora;
     private $estado;
 
-    public function __construct($idSala, $titulo, $descripcion, $modalidad, $nickNameCreador, $ubicacion = null, $fechaHora, $estado)
+    //LISTA ESTADOS: pendiente crear un enum si da el tiempo.
+    //EN_PREPARACION
+    //EN_CURSO
+    //FINALIZADA
+
+    public function __construct($idSala, $titulo, $descripcion, $modalidad, $nickNameCreador, $ubicacion, $fechaHora, $estado)
     {
-        $this->idSala = $idSala;
+        if($idSala == -1)
+        {
+            $this->idSala = 0; //Es auto increment en la base de datos, esta parte solo se setea cuando traes datos desde la base de datos.
+        }
+        else
+        {
+            $this->idSala = $idSala;
+        }
+
         $this->titulo = $titulo;
         $this->descripcion = $descripcion;
         $this->modalidad = $modalidad;
         $this->nickNameCreador = $nickNameCreador;
-        $this->ubicacion = $ubicacion;
         $this->fechaHora = $fechaHora;
-        $this->estado = $estado;
+
+        if($estado == "creando")
+        {
+            $this->estado = "EN_PREPARACION";
+        }
+        else
+        {
+            $this->estado = $estado;
+        }
+        
+        
+        if($modalidad == "Virtual")
+        {
+            $this->ubicacion = null;
+        }
+        else
+        {
+            $this->ubicacion = $ubicacion;
+        }
+        
+        
     }
 
     public function getIdSala()
@@ -61,6 +93,24 @@ class Sala
     public function getEstado()
     {
         return $this->estado;
+    }
+
+    public function registrarSala($conexion, $datosSala)
+    {   
+        if($this->idSala == 0)
+        {    
+            $instruccion = $conexion->prepare("INSERT INTO Sala (Titulo, Descripcion, Modalidad, Ubicacion, Fecha, nickname, Estado) VALUES (?, ?, ?, ?, ?, ?, ?)");
+            $instruccion->bind_param("sssssss", $this->titulo, $this->descripcion, $this->modalidad, $this->ubicacion, $this->fechaHora, $this->nickNameCreador, $this->estado);
+            
+            $resultado = $instruccion->execute(); //la ejecucion tambien retorna si salio bien o no.
+
+            return $resultado;  //retorna true si se registro, si no retorna false directamente.
+            
+        }
+        else
+        {
+            return false;
+        }
     }
 }
 
