2b3ca9fa7f185528757c3e485a84de1cd53ae2ce
Klaa5 <nic.n@outlook.es>
2026-05-10 00:37:18 -0300

    Se agrega nueva pagina para crear sala con formulario en primera version

---
+++ paginas/crearSala.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crear sala</title>
+    <link rel="icon" type="image/png" href="../assets/EventConnect.png">
+</head>
+<body style="background-color: lightblue;">
+    <form action="crearSalaServer.php" method="post">
+<h2>EventConnect - Crear nueva sala</h2><br><hr><br>
+    <p>Titulo de la sala:</p>
+    <input type="text" name="titulo" id="titulo"><br>
+    <p>Descripcion de la sala:</p>
+    <input type="text" name="descripcion" id="descripcion"><br>
+    <p>Modalidad:</p>
+    <select name="modalidad">
+        <option value="virtual">Virtual</option>
+        <option value="en_persona">En persona</option>
+    </select><br>
+    <p>Ubicacion:</p>
+    <input type="text" name="ubicacion" id=""ubicacion> <br>
+    <p>Fecha a realizar:</p>
+    <input type="datetime" name="fechaHora" id="fechaHora"><br><br>
+    <input type="submit" value="Crear">
+
+    </form>
+</body>
+</html>
