f5c072a2f3489467e8f21393dc6eaf7a904f0f6d
Klaa5 <nic.n@outlook.es>
2026-05-14 01:59:57 -0300

    Correcciones de lógica

--- control/paginaPrincipal.php
+++ control/paginaPrincipal.php
@@ -18,16 +18,18 @@
             $accesoDBSala = new salaBD();
             $salas = $accesoDBSala->getAllSalas($this->conexion);
 
-            echo "<form action='../paginas/visorSala.php' method='POST'>";           
-            echo "<hr>";
+            
             while ($fila = mysqli_fetch_assoc($salas)) 
             {
                 if($fila['Estado'] != "FINALIZADA" && $fila['Estado'] != "EN_CURSO")  //Solo mostrara las salas vigentes.
                 {
+                    echo "<form action='../paginas/visorSala.php' method='POST'>";           
+                    echo "<hr>";
                     echo "<input type='hidden' name='idSala' value='" . $fila['Id_sala'] . "'>";
                     echo "<button type='submit'>";
-                    
-                    echo "Sala: " . $fila['Titulo'] . " Modalidad: " . $fila['Modalidad'];
+                    echo "Sala: " . $fila['Titulo'];
+                    echo "<br>";
+                    echo "Modalidad: " . $fila['Modalidad'];
                     echo "</button>";
                     echo "</form>";
                     echo "<hr>";
