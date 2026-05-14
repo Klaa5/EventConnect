4f07b56d52cd4a5bca6e4aa79e15519dfec64402
Klaa5 <nic.n@outlook.es>
2026-05-14 01:38:37 -0300

    Agregada View de sala

---
+++ paginas/visorSala.php
@@ -0,0 +1,44 @@
+<?php   
+session_start();
+
+    if(empty($_SESSION['nickName']))   
+    {
+        header("Location: login.php");
+        exit();
+    }
+    
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EventConnect - Visor de Sala</title>
+</head>
+<body>
+    <?php
+        include "../control/salaManager.php";
+        $salaManager = new SalaManager($_POST['idSala']);
+        $sala = $salaManager->obtenerSala($_POST['idSala']);      
+    ?>
+
+    <h2>EventConnect - Visor de Sala</h2>
+    <br>
+    <hr>
+    <h3><?php echo "Sala: " . $sala->getTitulo() . " Modalidad: " . $sala->getModalidad(); ?></h3>
+    <hr>
+    <p>Descripción: <?php echo $sala->getDescripcion(); ?></p>
+    <hr>
+    <p>Participantes:</p>
+    <br>
+    <?php
+        foreach($sala->getParticipantes() as $participante)
+        {
+            echo "<p>" . $participante . "</p>";
+        }
+    ?>
+
+</body>
+</html>
