6688d0993a9b0d129df5902b10dddc4dbfe8c71e
Klaa5 <nic.n@outlook.es>
2026-05-14 01:34:33 -0300

    Agregada vista de salas

--- control/paginaPrincipal.php
+++ control/paginaPrincipal.php
@@ -2,7 +2,6 @@
     include "../database/conexion.php";
     include "../objetos/usuario.php";
     include "../database/accesoBD/salaBD.php";
-    session_start();
 
     class PaginaPrincipal
     {
@@ -19,12 +18,18 @@
             $accesoDBSala = new salaBD();
             $salas = $accesoDBSala->getAllSalas($this->conexion);
 
+            echo "<form action='../control/visorSala.php' method='POST'>";           
+            echo "<hr>";
             while ($fila = mysqli_fetch_assoc($salas)) 
             {
                 if($fila['Estado'] != "FINALIZADA" && $fila['Estado'] != "EN_CURSO")  //Solo mostrara las salas vigentes.
                 {
-                    echo "<hr>";
-                    echo "Sala: " . $fila['Titulo'] . " Modalidad: " . $fila['Modalidad'] . "<br>";
+                    echo "<input type='hidden' name='idSala' value='" . $fila['Id_sala'] . "'>";
+                    echo "<button type='submit'>";
+                    
+                    echo "Sala: " . $fila['Titulo'] . " Modalidad: " . $fila['Modalidad'];
+                    echo "</button>";
+                    echo "</form>";
                     echo "<hr>";
                 }
             }
