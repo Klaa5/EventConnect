1ff028d5f778259bc9bb0a4bf847caa4136d1a9b
Klaa5 <nic.n@outlook.es>
2026-05-13 22:53:23 -0300

    Correcciones menores

---
+++ control/verificacionLoginRegistro.php
@@ -0,0 +1,63 @@
+<?php
+    include "../database/conexion.php";
+    include "../objetos/usuario.php";
+    include "../database/accesoBD/usuarioBD.php";
+    session_start();
+
+    $conexion = new Conexion();
+    $conexion = $conexion->iniciarDB();
+   
+    $accesoUserBD = new UsuarioBD();
+
+    if($_POST["action"] == "Iniciar Sesión")
+    {
+        //Caso admin...
+        if($_POST['nickName'] == "admin" && $_POST['password'] == "admin")
+        {
+            $_SESSION['nickName'] = $_POST['nickName'];
+            header("Location: ../paginas/paginaPrincipal.php"); //pendiente ver que hacer con el admin...
+            exit();
+
+        }
+        
+        
+
+        if($accesoUserBD->buscarCuenta($_POST['nickName'], $_POST['password'], $conexion, 0))
+        {
+            $_SESSION['nickName'] = $_POST['nickName'];
+            header("Location: ../paginas/paginaPrincipal.php");
+            exit(); 
+        }
+        else
+        {
+            header("Location: ../paginas/login.php");
+            exit();
+        }
+    }
+    
+    
+    if($_POST["action"] == "Registrarse")
+    {
+        //verifico que no deje vacio y que esa cuenta no exista.
+        if(($_POST['nickNameReg'] != "" && $_POST['passwordReg'] != "") && !$accesoUserBD->buscarCuenta($_POST['nickNameReg'], "", $conexion, 1))
+        {
+            $usuario = new Usuario($_POST['nickNameReg'], $_POST['passwordReg'], null);
+            $usuario->registrarUsuario($_POST['nickNameReg'], $_POST['passwordReg'], $conexion);
+            header("Location: ../paginas/login.php");
+            exit();
+        }
+        else
+        {
+            header("Location: ../paginas/login.php");
+            exit();
+        }
+    }
+    
+
+
+
+
+
+        
+
+?>    
