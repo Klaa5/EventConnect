9c9b30d433efdb25891f83a2c6cf5b822e231d56
Klaa5 <nic.n@outlook.es>
2026-05-14 01:37:30 -0300

    Funcion getSala() mejorada

--- database/accesoBD/salaBD.php
+++ database/accesoBD/salaBD.php
@@ -1,15 +1,46 @@
 <?php
-
+include "../objetos/usuario.php";
+include "../objetos/sala.php";
 
     class salaBD
     {
         public function getSala($idSala, $conexion)
         {   
-            $consulta = "SELECT * FROM Sala WHERE Id_sala = ? NATURAL JOIN Participa WHERE Id_sala = ?"; 
+            $consulta = "SELECT * FROM Sala NATURAL JOIN Participa NATURAL JOIN Usuario WHERE Id_sala = ?"; 
             $instruccion = $conexion->prepare($consulta);
             $instruccion->bind_param("i", $idSala);
             $instruccion->execute();
-            return $instruccion->get_result();
+            $instruccion->get_result();
+
+            if($instruccion->get_result()->num_rows > 0)    //Si hay integrantes (no cuenta al creador) entonces se obtienen todos ellos
+            {
+                $primeraFila = $instruccion->fetch_assoc(); //Obtengo la primer fila.
+
+                //Esto ya itera al usar la funcion de arriba, asi que cuando empiece el while empieza desde el segundo integrante!
+                //Agrego los datos de la sala y del primer integrante.
+                $datosSala = new Sala($primeraFila["Id_sala"], $primeraFila["Titulo"], $primeraFila["Descripcion"], $primeraFila["Modalidad"], $primeraFila["nicknameCreador"], $primeraFila["Ubicacion"], $primeraFila["Fecha"], $primeraFila["Estado"]);
+                $datosSala->agregarParticipante($primeraFila['nickname']);  //Este es el primer participante, que casi se me pierde.
+
+                //Aca obtengo el nombre de cada participante.
+                while($fila = $instruccion->fetch_assoc())
+                {
+                    $datosSala->agregarParticipante($fila['nickname']);
+                }
+            }
+            else    //Si no hay integrantes, obtengo solo los datos de la sala y el nick del creador.
+            {
+                $consulta1 = "SELECT * FROM Sala WHERE Id_sala = ?"; 
+                $instruccion1 = $conexion->prepare($consulta1);
+                $instruccion1->bind_param("i", $idSala);
+                $instruccion1->execute();
+
+                $fila = $instruccion1->get_result()->fetch_assoc();
+                $datosSala = new Sala($fila["Id_sala"], $fila["Titulo"], $fila["Descripcion"], $fila["Modalidad"], $fila["nicknameCreador"], $fila["Ubicacion"], $fila["Fecha"], $fila["Estado"]);
+                
+            }
+            
+            return $datosSala;
+
         }
 
         public function getAllSalas($conexion)
