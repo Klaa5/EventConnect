3867ed0602767aa66be9dd30d9afb645b3227476
Klaa5 <nic.n@outlook.es>
2026-05-14 00:00:25 -0300

    Correcciones y mejoras de integridad

--- paginas/crearSala.php
+++ paginas/crearSala.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+
+    if(empty($_SESSION['nickName']))   
+    {
+        header("Location: login.php");
+        exit();
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- paginas/paginaPrincipal.php
+++ paginas/paginaPrincipal.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+
+    if(empty($_SESSION['nickName']))   
+    {
+        header("Location: login.php");
+        exit();
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,6 +21,9 @@
     <hr>
     <form action="crearSala.php">
         <button type="submit">Nueva sala</button>
+    </form>
+    <form action="../control/sessionManager.php" method="post">
+        <input type="submit" value="Cerrar Sesión" name="action">
     </form>
     
     <hr>
