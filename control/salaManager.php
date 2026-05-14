06b89da877bd966e2b43b30d778a7b694b424ca0
Klaa5 <nic.n@outlook.es>
2026-05-14 01:38:12 -0300

    Agregado manager de sala

---
+++ control/salaManager.php
@@ -0,0 +1,29 @@
+<?php
+    include "../database/conexion.php";
+    include "../objetos/usuario.php";
+    include "../database/accesoBD/salaBD.php";
+
+    class SalaManager
+    {
+        private $conexion;
+        private $idSala;
+
+        public function __construct($idSala)
+        {
+            $this->conexion = new Conexion();
+            $this->conexion = $this->conexion->iniciarDB();
+            $this->idSala = $idSala;
+        }
+
+        public function obtenerSala($idSala)
+        {
+            $accesoDBSala = new salaBD();
+            $sala = $accesoDBSala->getSala($idSala, $this->conexion);
+            return $sala;
+        }
+    }
+
+    
+
+
+?>
