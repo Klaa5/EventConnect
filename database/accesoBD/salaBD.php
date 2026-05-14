a6afd1f8d38239cd4709ff1e4069a9adc614bdda
Klaa5 <nic.n@outlook.es>
2026-05-13 23:30:18 -0300

    Se agrega nueva función para registro de salas en base de datos.

--- database/accesoBD/salaBD.php
+++ database/accesoBD/salaBD.php
@@ -3,11 +3,29 @@
 
     class salaBD
     {
-        function getAllSalas($conexion)
+        public function getAllSalas($conexion)
         {
             $consulta = "SELECT * FROM Sala";
             return mysqli_query($conexion, $consulta); 
         }  
+
+        public function registrarSala($conexion, $datosSala)
+        {   
+            if($datosSala->getIdSala() == null || $datosSala->getIdSala() == -1) //Si es un objeto dirigido a ser almacenado (se envia como -1 o null))...
+            {    
+                $instruccion = $conexion->prepare("INSERT INTO Sala (Titulo, Descripcion, Modalidad, Ubicacion, Fecha, nickname, Estado) VALUES (?, ?, ?, ?, ?, ?, ?)");
+                $instruccion->bind_param("sssssss", $datosSala->getTitulo(), $datosSala->getDescripcion(), $datosSala->getModalidad(), $datosSala->getUbicacion(), $datosSala->getFechaHora(), $datosSala->getNickNameCreador(), $datosSala->getEstado());
+                
+                $resultado = $instruccion->execute(); //la ejecucion tambien retorna si salio bien o no.
+
+                return $resultado;  //retorna true si se registro, si no retorna false directamente.
+                
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 
 
