8b98782570e736199825bf58d9abc9b70a9a11b9
Klaa5 <nic.n@outlook.es>
2026-05-13 23:31:22 -0300

    Mejoras en muestra de salas

--- control/paginaPrincipal.php
+++ control/paginaPrincipal.php
@@ -1,17 +1,37 @@
 <?php
-    
     include "../database/conexion.php";
     include "../objetos/usuario.php";
     include "../database/accesoBD/salaBD.php";
     session_start();
 
-    $conexion = new Conexion();
-    $conexion = $conexion->iniciarDB();
-
-    function mostrarSalas($salas)
+    class PaginaPrincipal
     {
-        
-    }
+        private $conexion;
 
+        public function __construct()
+        {
+            $this->conexion = new Conexion();
+            $this->conexion = $this->conexion->iniciarDB();
+        }
+
+        public function mostrarSalas()
+        {
+            $accesoDBSala = new salaBD();
+            $salas = $accesoDBSala->getAllSalas($this->conexion);
+
+            while ($fila = mysqli_fetch_assoc($salas)) 
+            {
+                echo "<hr>";
+                echo "Sala: " . $fila['Titulo'] . " Modalidad: " . $fila['Modalidad'] . "<br>";
+                echo "<hr>";
+            }
+
+        }
+
+
+    }
+    
+
+    
 
 ?>
