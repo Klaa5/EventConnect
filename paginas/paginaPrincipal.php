4d9354e446fe82ff3f343d17120f7bcc386fafc1
Klaa5 <nic.n@outlook.es>
2026-06-03 21:52:54 -0300

    Agrego referencia a CSS en los link de bootstrap

--- paginas/paginaPrincipal.php
+++ paginas/paginaPrincipal.php
@@ -19,7 +19,7 @@ if(empty($_SESSION['nickName']))
 
     <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="../assets/css/estilos.css">
+    <link rel="stylesheet" href="../assets/CSS/estilos.css">
 </head>
 
 <body>
--- paginas/visorSala.php
+++ paginas/visorSala.php
@@ -19,7 +19,7 @@ if(empty($_SESSION['nickName']))
 
     <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 
-    <link href="../assets/css/estilos.css" rel="stylesheet">
+    <link href="../assets/CSS/estilos.css" rel="stylesheet">
 </head>
 
 <body>
