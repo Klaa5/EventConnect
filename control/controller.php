0d164d92f1ffcb5aeeb7263a5a332d2cca622eb8
Klaa5 <nic.n@outlook.es>
2026-06-10 18:30:47 -0300

    Se agregan funciones de salaManager al controller.

--- control/controller.php
+++ control/controller.php
@@ -8,6 +8,35 @@
     $conexion = new Conexion();
     $conexion = $conexion->iniciarDB();
 
+    if($_POST["action"] == "Crear Sala")    //Crear sala.
+    {
+        if(!empty($_SESSION['nickName']))
+        {
+            $salaManager = new SalaManager($_SESSION['nickName']);
+            
+            if($salaManager->crearSala($_POST))
+            {
+                //PENDIENTE VER COMO DAR UN MENSAJE DE SUCCESS
+                header("Location: ../paginas/paginaPrincipal.php"); //redirige a la pagina principal si todo sale bien
+                exit();
+            }
+            else
+            {
+                //PENDIENTE VER COMO DAR MENSAJE DE ERROR  
+                header("Location: ../paginas/crearSala.php");   //vuelve a la pagina de creacion a reintentar.
+                exit();
+            }
+
+        }
+        else
+        {
+            header("Location: ../paginas/login.php");
+            exit();
+        }
+       
+    }
+
+    
 
 
 
