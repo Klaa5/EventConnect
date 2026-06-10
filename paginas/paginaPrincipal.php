b457811e1596e1611b9d1983df9df12e17bca6b4
Klaa5 <nic.n@outlook.es>
2026-06-10 19:35:08 -0300

    Debug test - new pages to old ones (temp)

--- paginas/paginaPrincipal.php
+++ paginas/paginaPrincipal.php
@@ -1,81 +1,39 @@
 <?php
 session_start();
 
-if(empty($_SESSION['nickName']))
-{
-    header("Location: login.php");
-    exit();
-}
+    if(empty($_SESSION['nickName']))   
+    {
+        header("Location: login.php");
+        exit();
+    }
 ?>
 
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EventConnect - Principal</title>
-
     <link rel="icon" type="image/png" href="../assets/EventConnect.png">
-
-    <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;600;700;800&display=swap" rel="stylesheet">
-
-    <link rel="stylesheet" href="../assets/CSS/estilos.css">
 </head>
-
-<body>
+<body style="background-color: lightblue;">
+    <h2>EventConnect - main</h2><br><br>
+    <hr>
+    <form action="crearSala.php">
+        <button type="submit">Nueva sala</button>
+    </form>
+    <form action="../control/sessionManager.php" method="post">
+        <input type="submit" value="Cerrar Sesión" name="action">
+    </form>
     
-    <aside class="sidebar">
+    <hr>
+    <h3>Salas disponibles:</h3>
+    <?php
+        include "../control/paginaPrincipal.php";
+        $paginaPrincipal = new PaginaPrincipal();
+        $paginaPrincipal->mostrarSalas();
+    ?>
 
-        <div class="sidebar-header">
-            <img src="../assets/EventConnect.png" alt="Logo" class="logo-sidebar">
-            <h2>EventConnect</h2>
-        </div>
-
-        <form action="crearSala.php">
-            <button type="submit" class="btn-sidebar">
-                + Nueva Sala
-            </button>
-        </form>
-
-        <div class="sidebar-user">
-            Bienvenido<br>
-            <strong><?php echo $_SESSION['nickName']; ?></strong>
-        </div>
-
-        <form action="../control/sessionManager.php" method="post" class="logout-form">
-            <button
-                type="submit"
-                name="action"
-                value="Cerrar Sesión"
-                class="btn-logout">
-
-                Cerrar Sesión
-
-            </button>
-        </form>
-
-    </aside>
-
-    <main class="main-content">
-
-        <div class="glass-card">
-
-            <h1>Salas Disponibles</h1>
-
-            <div class="salas-listado">
-
-                <?php
-                    include "../control/paginaPrincipal.php";
-
-                    $paginaPrincipal = new PaginaPrincipal();
-                    $paginaPrincipal->mostrarSalas();
-                ?>
-
-            </div>
-
-        </div>
-
-    </main>
 
 </body>
 </html>
