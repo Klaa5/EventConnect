08669376a026e1c8d1fde3d3a67604b3cf44053b
Klaa5 <nic.n@outlook.es>
2026-06-10 22:24:13 -0300

    Descartadas variables de debug mode

--- index.php
+++ index.php
@@ -1,30 +1,15 @@
 <?php
     session_start();
-
+    
     if(!empty($_SESSION['nickName']))   //Al ingresar verifico si el usuario se ha logueado.
     {
-        if($_SESSION['oldHtml'] == true)  
-        {
-            header("Location: paginas/old/paginaPrincipal.php");
-        }
-        else
-        {
-            header("Location: paginas/paginaPrincipal.php");
-        }
+        header("Location: paginas/paginaPrincipal.php");
         exit();
     }
     else
     {
-        if($_SESSION['oldHtml'] == true)  
-        {
-            header("Location: paginas/old/login.php");
-        }
-        else
-        {
-            header("Location: paginas/login.php");
-        }
+        header("Location: paginas/login.php");
         exit();
-       
     }
     
 ?>
