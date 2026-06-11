c80c1c8259c0d01db21cf50b6901a12a93953666
Klaa5 <nic.n@outlook.es>
2026-06-11 00:42:41 -0300

    Implementada funcion para obtener datos de un solo usuario

--- control/accountManager.php
+++ control/accountManager.php
@@ -1,5 +1,7 @@
 <?php
-    include "../database/accesoBD/usuarioBD.php";
+    include_once "../database/accesoBD/usuarioBD.php";
+    include_once "../database/conexion.php";
+    include_once "../objetos/usuario.php";
     
     class accountManager
     {
@@ -21,6 +23,11 @@
         public function registrarUser($usuario)
         {
             return $this->accesoUserBD->registrarUsuario($usuario, $this->conexion);
+        }
+
+        public function obtenerDatosUsuario($nickName)
+        {
+            return $this->accesoUserBD->obtenerDatosUsuario($nickName, $this->conexion);
         }
 
     }
