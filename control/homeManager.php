0f2313a3a62ba94009355ee02bdde0fc2aab43d1
Klaa5 <nic.n@outlook.es>
2026-06-17 05:10:20 -0300

    Se agrega html de salas encontradas

--- control/homeManager.php
+++ control/homeManager.php
@@ -59,7 +59,66 @@
         }
 
 
+        public function buscarSala($ingreso)
+        {
+            $accesoDBSala = new salaBD();
+            $salasFiltradas = $accesoDBSala->getSalasFiltradas($this->conexion, $ingreso);           
+
+            if($salasFiltradas != null || !empty($salasFiltradas))
+            {
+                $pag = './paginaPrincipal.php';
+
+                echo '<form action=' . $pag . ' method="post"> <input type="submit" value="Volver a pagina principal"> </form>';
+
+                echo "<div class='salas-listado'>";
+
+                while($sala = mysqli_fetch_assoc($salasFiltradas))
+                {
+                    if($sala['Estado'] != "FINALIZADA" && $sala['Estado'] != "EN_CURSO")
+                    {
+                        if($sala['Modalidad'] == "virtual")
+                        {
+                            $mod = "🌐 Evento Online / Virtual";
+                        }
+                        else
+                        {
+                            $mod = "";
+                        }
+
+                        echo "
+                        <a href='../paginas/visorSala.php?idSala=".$sala['Id_sala']."'
+                        style='text-decoration:none;color:inherit;'>
+
+                            <div class='sala-card'>
+
+                                <div class='sala-titulo'>
+                                    ".$sala['Titulo']."
+                                </div>
+
+                                <div class='sala-info'>
+                                    Fecha de inicio:
+                                    ".date("d/m/Y H:i", strtotime($sala['Fecha']))."
+                                    <br><br>
+                                    ".$mod."
+                                </div>
+
+                            </div>
+
+                        </a>";
+                    }
+                }
             }
+            else
+            {
+                echo "<div class='salas-listado'>";
+                echo '<p no se han encontrado coincidencias p>';
+            }
+
+            echo "</div>";
+        }
+
+
+    }
             
 
             
