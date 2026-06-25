ca65843bca3d2e80010a318392bbf05dabac282f
Klaa5 <nic.n@outlook.es>
2026-06-25 17:05:01 -0300

    Implementada compatibilidad con server de clase

--- database/conexion.php
+++ database/conexion.php
@@ -4,27 +4,38 @@
     class Conexion
     {
         private $host = "localhost";
-        private $user = "user";
-        private $password = "user123";
-        private $bd = "EventConnect";
+        private $user = ["user", "sitio_equipodos"];
+        private $password = ["user123", "equipodos"];
+        private $bd = ["EventConnect", "sitio_EventConnect"];
         private $conexion;  
 
 
         public function iniciarDB()
         {
-            try
+            
+            for ($i = 0; $i < 2; $i++) //Intento con las dos BD
             {
-                $this->conexion = new mysqli($this->host, $this->user, $this->password, $this->bd);
-
-                if($this->eventStateUpdaterDetector())
+                try
                 {
-                    @$this->conexion->query("SET GLOBAL event_scheduler = ON;");
+                    $this->conexion = new mysqli($this->host, $this->user[$i], $this->password[$i], $this->bd[$i]);
+
+                    break;  //Llega aca si se logra la conexion
                 }
-                
+                catch (mysqli_sql_exception)
+                {
+                    $this->conexion = null;
+                }
             }
-            catch(mysqli_sql_exception)
+
+            if ($this->conexion == null)    //return
             {
-                echo "Error al conectar a la bd";
+                echo "Error, no se puede conectar a las bd";
+                return null;
+            }
+
+            if($this->eventStateUpdaterDetector())
+            {
+                @$this->conexion->query("SET GLOBAL event_scheduler = ON;");
             }
 
             return $this->conexion;
