5017d67b3c9ca96863403fdfd858d7d7b0c81c1b
Klaa5 <nic.n@outlook.es>
2026-06-25 16:45:26 -0300

    Mejoras y manejo de errores en bases de datos ajenas

--- database/conexion.php
+++ database/conexion.php
@@ -15,7 +15,11 @@
             try
             {
                 $this->conexion = new mysqli($this->host, $this->user, $this->password, $this->bd);
-                @$this->conexion->query("SET GLOBAL event_scheduler = ON;");
+
+                if($this->eventStateUpdaterDetector())
+                {
+                    @$this->conexion->query("SET GLOBAL event_scheduler = ON;");
+                }
                 
             }
             catch(mysqli_sql_exception)
@@ -24,6 +28,23 @@
             }
 
             return $this->conexion;
+        }
+
+        public function eventStateUpdaterDetector()
+        {
+            $resultado = $this->conexion->query("SHOW EVENTS");
+
+            if($resultado != false)
+            {
+                if($resultado->num_rows > 0)
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            return false;
         }
 
 
