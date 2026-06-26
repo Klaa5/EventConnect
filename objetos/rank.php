2464a9e0db61d3c36cfef4f59c00bd72c8b64568
Klaa5 <nic.n@outlook.es>
2026-06-26 16:42:32 -0300

    Formato corregido

--- objetos/rank.php
+++ objetos/rank.php
@@ -1,52 +1,55 @@
 <?php
 
-class rank
-{
-    private $idRank;
-    private $nickNameEvaluado;
-    private $nickNameEvaluador;
-    private $idSala;
-    private $puntaje;
-
-    public function __construct($idRank, $nickNameEvaluado, $nickNameEvaluador, $idSala, $puntaje)
+    class rank
     {
-        $this->idRank = $idRank;
-        $this->nickNameEvaluado = $nickNameEvaluado;
-        $this->nickNameEvaluador = $nickNameEvaluador;
-        $this->idSala = $idSala;
-        
-        if($puntaje == null)
+        private $idRank;
+        private $nickNameEvaluado;
+        private $nickNameEvaluador;
+        private $idSala;
+        private $puntaje;
+
+        public function __construct($idRank, $nickNameEvaluado, $nickNameEvaluador, $idSala, $puntaje)
         {
-            $this->puntaje = 0;
+            $this->idRank = $idRank;
+            $this->nickNameEvaluado = $nickNameEvaluado;
+            $this->nickNameEvaluador = $nickNameEvaluador;
+            $this->idSala = $idSala;
+            
+            if($puntaje == null)
+            {
+                $this->puntaje = 0;
+            }
+            else
+            {
+                $this->puntaje = $puntaje;
+            }
         }
-        else
+
+        public function getIdRank()
         {
-            $this->puntaje = $puntaje;
+            return $this->idRank;
         }
-    }
 
-    public function getIdRank()
-    {
-        return $this->idRank;
-    }
+        public function getNickNameEvaluado()
+        {
+            return $this->nickNameEvaluado;
+        }
 
-    public function getNickNameEvaluado()
-    {
-        return $this->nickNameEvaluado;
-    }
+        public function getNickNameEvaluador()
+        {
+            return $this->nickNameEvaluador;
+        }
 
-    public function getNickNameEvaluador()
-    {
-        return $this->nickNameEvaluador;
-    }
+        public function getIdSala()
+        {
+            return $this->idSala;
+        }
 
-    public function getIdSala()
-    {
-        return $this->idSala;
-    }
+        public function getPuntaje()
+        {
+            return $this->puntaje;
+        }
 
-    public function getPuntaje()
-    {
-        return $this->puntaje;
     }
-}
+    
+?>
